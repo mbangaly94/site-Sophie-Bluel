1e945d2e5aeafc7bf4934234e9b5130732ca9b31
SYLLABangaly <bangalysylla123@gmail.com>
2024-07-30 23:35:37 +0200

    ending of loging step

---
+++ FrontEnd/login.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const email = document.querySelector("form #email");
+  const password = document.querySelector("form #password");
+  const form = document.querySelector("form");
+  const errorMsg = document.querySelector("#erreur-msg");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    errorMsg.textContent = ""; // Réinitialiser le message d'erreur
+
+    fetch("http://localhost:5678/api/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email.value,
+        password: password.value,
+      }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Erreur dans l’identifiant ou le mot de passe");
+        }
+      })
+      .then((data) => {
+        // Traitez les données de réponse ici
+        console.log(data);
+      })
+      .catch((error) => {
+        // Afficher un message d'erreur seulement si la réponse n'est pas correcte
+        errorMsg.textContent = error.message;
+        console.error(error);
+      });
+  });
+});
